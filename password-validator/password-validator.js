19cbe8efdf13535ec9eefb68522ca808b14d8f68
Aaron Harpt <a169h161@ku.edu>
2021-04-14 11:20:23 -0500

    Add checkPasswordsMatch() function

--- password-validator/password-validator.js
+++ password-validator/password-validator.js
@@ -1,15 +1,28 @@
 // make sure password length is at least 8 characters
 let checkPasswordLength = () => {
-  if (document.getElementById("input1").innerText.length < 8) {
+  if (document.getElementById("input1").value.length < 8) {
     return false;
   }
 
   return true;
 };
 
+let checkPasswordsMatch = () => {
+  if (document.getElementById("input1").value === document.getElementById("input2").value) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 
 let validatePassword = () => {
+  let errorParagraph = "";
   if (!checkPasswordLength()) {
-    document.body.innerHTML += "<p class='error'>Please give a password that is at least 8 characters long</p>";
+    errorParagraph = "<p class='error'>Please give a password that is at least 8 characters long</p>";
+    document.body.innerHTML += errorParagraph;
+  } else if (!checkPasswordsMatch()) {
+    errorParagraph = "<p class='error'>Your passwords do not match</p>";
+    document.body.innerHTML += errorParagraph;
   }
 };
